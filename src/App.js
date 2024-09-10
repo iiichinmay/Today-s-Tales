3f63c340c3ffcf22ea41c90be7be6b2a24314b23
Chinmay <ichinmay2461@gmail.com>
2024-09-10 20:05:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="nav">
+        <div className="leftnav">
+          <a href="#">Home</a>
+          <a href="#">Account</a>
+          <a href="#">Mens Fashion</a>
+          <a href="#">Women Fashion</a>
+        </div>
+        <div className="rightnav">
+          <a href="#">Signup</a>
+          <a href="#">Login</a>
+        </div>
+      </div>
+      <header className="App-header">
+        <p>Welcome to the React App!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
